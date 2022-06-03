fd0fd79e47131f4f1f2c572142a6699a936c678b
rosieett <58713682+rosieett@users.noreply.github.com>
2022-06-03 11:17:45 -0400

    adding in my array of cookie info

---
+++ app.js
@@ -0,0 +1,93 @@
+var categories = [{
+        name: 'Tenderness',
+        id: 'tenderness',
+        description: 'Heirloom biodiesel quinoa mlkshk asymmetrical master cleanse affogato blog marfa chillwave semiotics gastropub',
+        imgUrl: '',
+        options: [{
+                label: 'Crunchy',
+                value: 'crunchy',
+                imgUrl: '',
+            },
+            {
+                label: 'Soft',
+                value: 'soft',
+                imgUrl: '',
+            },
+        ],
+    },
+    {
+        name: 'Rise',
+        id: 'rise',
+        description: 'Offal waistcoat occupy humblebrag pinterest, pour-over food truck bitters.',
+        imgUrl: '',
+        options: [{
+                label: 'Thick',
+                value: 'thick',
+                imgUrl: '',
+            },
+            {
+                label: 'Thin',
+                value: 'thin',
+                imgUrl: '../src/assets/images/Cookies-10.png',
+            },
+        ],
+    },
+    {
+        name: 'Texture',
+        id: 'texture',
+        description: 'Banjo flannel pour-over fixie twee humblebrag.',
+        imgUrl: '',
+        options: [{
+                label: 'Fudgy',
+                value: 'fudgy',
+                imgUrl: '',
+            },
+            {
+                label: 'Cakey',
+                value: 'cakey',
+                imgUrl: '',
+            },
+        ],
+    },
+    {
+        name: 'Chocolate',
+        id: 'chocolate',
+        description: 'Mixtape edison bulb tattooed blog, brunch seitan live-edge plaid forage',
+        imgUrl: '',
+        options: [{
+                label: 'Small Chunks',
+                value: 'small',
+                imgUrl: '',
+            },
+            {
+                label: 'Large Chunks',
+                value: 'large',
+                imgUrl: '',
+            },
+            {
+                label: 'Swirled In',
+                value: 'swirled',
+                imgUrl: '',
+            },
+        ],
+    },
+    {
+        name: 'Flavor',
+        id: 'flavor',
+        description: 'Sartorial single-origin coffee VHS green juice salvia.',
+        imgUrl: '',
+        options: [{
+                label: 'Dark, toffee',
+                value: 'dark',
+                imgUrl: '',
+            },
+            {
+                label: 'Light, milky',
+                value: 'light',
+                imgUrl: '',
+            },
+        ],
+    },
+]
+
+
